83e5ead8adb651cde38b459bcddeff458e421acd
Yipsi <yipsiavila437@gmail.com>
2019-07-10 19:25:20 -0600

    Primer commit
    creacion de base de datos

---
+++ docs/scripts/02_examen_data_script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `yipsiavila`.`paginas` (
+  `yyam_codigo` BIGINT(13) NULL AUTO_INCREMENT,
+  `yyam_plugin` VARCHAR(128) NULL,
+  `yyam_estado` CHAR(3) NULL,
+  `yyam_urlhomepage` VARCHAR(128) NULL,
+  `yyam_urlcdn` VARCHAR(128) NULL,
+  PRIMARY KEY (`yyam_codigo`));
