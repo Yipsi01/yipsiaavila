89e471dd5d4a3758c53d39f91e4263c1b43127f7
Yipsi <yipsiavila437@gmail.com>
2019-07-10 20:34:02 -0600

    Tercer Commit
    Trabajo en models y parte de view

--- models/examendata.model.php
+++ models/examendata.model.php
@@ -3,5 +3,113 @@ require_once "libs/dao.php";
 
 // Elaborar el algoritmo de los solicitado aquí.
 
+/*SELECT `paginas`.`yyam_codigo`,
+    `paginas`.`yyam_plugin`,
+    `paginas`.`yyam_estado`,
+    `paginas`.`yyam_urlhomepage`,
+    `paginas`.`yyam_urlcdn`
+FROM `yipsiavila`.`paginas`;*/
+
+
+* @return Array
+*/
+function obtenerListas()
+{
+   $sqlstr = "select `paginas`.`yyam_codigo`,
+   `paginas`.`yyam_plugin`,
+   `paginas`.`yyam_estado`,
+   `paginas`.`yyam_urlhomepage`,
+   `paginas`.`yyam_urlcdn`
+FROM `yipsiavila`.`paginas`";
+
+   $modas = array();
+   $modas = obtenerRegistros($sqlstr);
+   return $modas;
+}
+
+function obtenerPaginasPorId($id)
+{
+ $sqlstr="select `paginas`.`yyam_codigo`,
+ `paginas`.`yyam_plugin`,
+ `paginas`.`yyam_estado`,
+ `paginas`.`yyam_urlhomepage`,
+ `paginas`.`yyam_urlcdn`
+FROM `yipsiavila`.`paginas`where yyam_codigo=%d";
+
+ $paginas= array();
+ $paginas=obtenerUnRegistro(sprintf($sqlstr, $id));
+ return $paginas;
+}
+
+function obtenerEstadoPorId($id)
+{
+ $sqlstr="select `paginas`.`yyam_estado`
+       from `yipsiavila`.`paginas` where yyam_codigo=%d";
+ $paginas= array();
+ $paginas=obtenerUnRegistro(sprintf($sqlstr, $id));
+ return $paginas;
+}
+
+
+function obtenerEstados()
+{
+   return array(
+       array("cod"=>"ACT", "dsc"=>"Activo"),
+       array("cod"=>"INA", "dsc"=>"Inactivo"),
+       array("cod"=>"PLN", "dsc"=>"En Planificación"),
+       array("cod"=>"RET", "dsc"=>"Retirado"),
+       array("cod"=>"SUS", "dsc"=>"Suspendido"),
+       array("cod"=>"DES", "dsc"=>"Descontinuado")
+   );
+}
+
+function agregarNuevaPaginas($yyam_plugin, $yyam_estado, $yyam_urlhomepage, $yyam_urlcdn) {
+   $insSql = "INSERT INTO paginas(yyam_plugin, yyam_estado, yyam_urlhomepage,  yyam_urlcdn)
+     values ('%s','%s','%s','%s');";
+     if (ejecutarNonQuery(
+         sprintf(
+             $insSql,
+             $yyam_plugin,
+             $yyam_estado,
+             $yyam_urlhomepage,
+             $yyam_urlcdn
+
+         )))
+     {
+       return getLastInserId();
+     } else {
+         return false;
+     }
+}
+
+function modificarPaginas($yyam_plugin,  $yyam_estado,$yyam_urlhomepage,$yyam_urlcdn, $yyam_codigo)
+{
+   $updSQL = "UPDATE paginas set yyam_plugin='%s',
+   yyam_estado='%s',  yyam_urlhomepage='%s',yyam_urlcdn='%s' where yyam_codigo=%d;";
+
+   return ejecutarNonQuery(
+       sprintf(
+           $updSQL,
+           $yyam_plugin,
+           $yyam_estado,
+           $yyam_urlhomepage,
+           $yyam_urlcdn,
+           $yyam_codigo
+       )
+   );
+}
+function eliminarPaginas($idjuguete)
+{
+   $delSQL = "DELETE FROM paginas where yyam_codigo=%d;";
+
+   return ejecutarNonQuery(
+       sprintf(
+           $delSQL,
+           $yyam_codigo
+       )
+   );
+}
+?>
+
 
 ?>
--- views/examenform.view.tpl
+++ views/examenform.view.tpl
@@ -1,12 +1,75 @@
-<h1>Trabajando con: </h1>
-<hr>
-<div class="row">
-  <form class="col-6 col-offset-3">
+<h1>{{modeDsc}}</h1>
+<section class="row">
+<form action="index.php?page=examenform" method="post" class="col-8 col-offset-2">
+  {{if hasErrors}}
+    <section class="row">
+      <ul class="error">
+        {{foreach errores}}
+          <li>{{this}}</li>
+        {{endfor errores}}
+      </ul>
+    </section>
+  {{endif hasErrors}}
+  <input type="hidden" name="mode" value="{{mode}}"/>
+  <input type="hidden" name="xcfrt" value="{{xcfrt}}" />
+  <input type="hidden" name="btnConfirmar" value="Confirmar" />
+  {{if showIdPaginas}}
+  <fieldset class="row">
+    <label class="col-5" for="yyam_codigo">Código de Pagina</label>
+    <input type="text" name="yyam_codigo" id="yyam_codigo" readonly value="{{yyam_codigo}}" class="col-7" />
+  </fieldset>
+  {{endif showIdPaginas}}
+  <fieldset class="row">
+    <label class="col-5" for="yyam_plugin">Nombre: </label>
+    <input type="text" name="yyam_plugin" id="yyam_plugin" {{readonly}} value="{{yyam_plugin}}" class="col-7" />
+  </fieldset>
 
-    <fieldset>
-      <button type="submit">Confirmar</button>
+  <fieldset class="row">
+    <label class="col-5" for="yyam_estado">Estado</label>
+    <select name="yyam_estado" id="yyam_estado" class="col-7" {{selectDisable}} {{readonly}} >
+      {{foreach yyam_estado}}
+        <option value="{{cod}}" {{selected}}>{{dsc}}</option>
+      {{endfor yyam_estado}}
+    </select>
+  </fieldset>
+
+  <fieldset class="row">
+    <label class="col-5" for="yyam_urlhomepage">URL: </label>
+    <input type="text" name="yyam_urlhomepage" id="yyam_urlhomepage" {{readonly}} value="{{yyam_urlhomepage}}" class="col-7" />
+  </fieldset>
+
+  <fieldset class="row">
+    <label class="col-5" for="yyam_urlcdn">CDN: </label>
+    <input type="text" name="yyam_urlcdn" id="yyam_urlcdn" {{readonly}} value="{{yyam_urlcdne}}" class="col-7" />
+  </fieldset>
+
+
+
+  <fieldset class="row">
+    <div class="right">
+      {{if showBtnConfirmar}}
+      <button type="button" id="btnConfirmar" >Confirmar</button>
       &nbsp;
-      <button type="submit">Cancelar</button>
-    </fieldset>
-  </form>
-</div>
+      {{endif showBtnConfirmar}}
+      <button type="button" id="btnCancelar">Cancelar</button>
+    </div>
+  </fieldset>
+
+  
+</form>
+</section>
+<script>
+  $().ready(function(){
+    $("#btnCancelar").click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      location.assign("index.php?page=examenlist");
+    });
+    $("#btnConfirmar").click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      /*Aqui deberia hacer validación de datos*/
+      document.forms[0].submit();
+    });
+  });
+</script>
